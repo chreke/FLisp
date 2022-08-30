502124862a6ce229351bcef0f44b3bd85517d9ee
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-08-30 09:50:49 +0200

    Update tests

--- Flisp.Tests/Tests.fs
+++ Flisp.Tests/Tests.fs
@@ -8,6 +8,13 @@ let unlift =
     | Ok v -> v
     | Error e -> e
 
+let isOk =
+    function
+    | Ok _ -> true
+    | Error _ -> false
+
+
+
 [<Fact>]
 let ``My test`` () = Assert.True(true)
 
@@ -16,6 +23,7 @@ let ``My test`` () = Assert.True(true)
 [<InlineData("true")>]
 [<InlineData("foo")>]
 [<InlineData("1")>]
+[<InlineData("\"foo\"")>]
 [<InlineData("(+ 1 2 3)")>]
 [<InlineData("'(+ 1 2 3)")>]
 let ``read program and then print it back`` program =
@@ -26,6 +34,16 @@ let ``read program and then print it back`` program =
     Assert.Equal(program, unlift result)
 
 [<Theory>]
+[<InlineData("\"foo")>]
+[<InlineData("foo\"")>]
+[<InlineData("(+ 1 2")>]
+[<InlineData("+ 1 2)")>]
+let ``read an invalid program and return an Error result`` program =
+    let result = Reader.read program
+    Assert.False(isOk result)
+
+[<Theory>]
+[<InlineData("\"foo\"", "\"foo\"")>]
 [<InlineData("true", "true")>]
 [<InlineData("'(1 2)", "(1 2)")>]
 [<InlineData("'foo", "foo")>]
